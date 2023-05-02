4b8c77c919a2b649d0cb127c0026f7bd5f9582dc
ManjunathanRajan <manjun566@gmail.com>
2023-05-02 09:50:56 +0530

    use formatting

--- create_github_release.sh
+++ create_github_release.sh
@@ -5,6 +5,6 @@ set -e
 NEW_TAG="$1"
 GITHUB_PAT="$2"
 RELEASE_TITLE="Golden Image Windows Server 2019 $NEW_TAG"
-RELEASE_DESCRIPTION="This is the release for Golden Image Windows Server 2019 $NEW_TAG. This release includes the latest updates and improvements.\n\nDefault region:us-west-2\n- EC2 > AMIs > Filter by Private images"
+RELEASE_DESCRIPTION="This is the release for Golden Image Windows Server 2019 $NEW_TAG. This release includes the latest updates and improvements.\n\n- Default region:us-west-2\n- EC2 > AMIs > Filter by Private images"
 
 curl -X POST -H "Authorization: token $GITHUB_PAT" -H "Content-Type: application/json" -d "{\"tag_name\":\"$NEW_TAG\",\"target_commitish\":\"main\",\"name\":\"$RELEASE_TITLE\",\"body\":\"$RELEASE_DESCRIPTION\"}" https://api.github.com/repos/ManjunathanRajan/golden-image-windows/releases
